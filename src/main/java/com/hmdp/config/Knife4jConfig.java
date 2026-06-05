d8b62421817d1b6149a2a1eea0979d0127a3db7d
Ethan Zhao <ethan1.zhao@momenta.ai>
2026-06-05 19:28:07 +0800

    feat: add Knife4j Swagger configuration

---
+++ src/main/java/com/hmdp/config/Knife4jConfig.java
@@ -0,0 +1,34 @@
+package com.hmdp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+@Configuration
+@EnableSwagger2WebMvc
+public class Knife4jConfig {
+
+    @Bean
+    public Docket defaultApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hmdp.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("大众点评API文档")
+                .description("大众点评项目接口文档")
+                .version("1.0")
+                .build();
+    }
+}
